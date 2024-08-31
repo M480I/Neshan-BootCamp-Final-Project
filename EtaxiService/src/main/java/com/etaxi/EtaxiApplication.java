17a316305285559af5d0786020284e4229795657
M480I <mah0041121@gmail.com>
2024-08-31 12:29:14 +0330

    initialize and add configurations to application.yml

---
+++ EtaxiService/src/main/java/com/etaxi/EtaxiApplication.java
@@ -0,0 +1,13 @@
+package com.etaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtaxiApplication.class, args);
+	}
+
+}
---
+++ EtaxiService/src/test/java/com/etaxi/EtaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
